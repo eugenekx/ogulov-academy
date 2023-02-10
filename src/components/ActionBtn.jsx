f7b580f155cb6da48e6d7a5c0b515cd07d3bbefb
minaylosd <minaylosd@gmail.com>
2023-02-10 17:47:33 +0400

    fix menu closing, fix btn size

--- src/components/ActionBtn.jsx
+++ src/components/ActionBtn.jsx
@@ -5,11 +5,11 @@ function ActionBtn ({children, withArrow, onClick, ...props}) {
         <button
         onClick={onClick}
         className={ withArrow ?
-            "lg:pl-8 xl:h-16 lg:h-14 h-10 pl-6 pr-5 flex items-center bg-yellow rounded-full lg:text-l text-m font-mabry uppercase"
+            "xl:pr-5 lg:pl-8 lg:pr-2 pl-6 pr-1  xl:h-16 lg:h-14 h-10 flex items-center bg-yellow rounded-full lg:text-l text-m font-mabry uppercase"
             :"lg:pl-8 lg:pr-8 xl:h-16 lg:h-14 h-10 pl-6 pr-6 flex items-center bg-yellow rounded-full lg:text-l text-m font-mabry uppercase"
             }>
             {children}
-            {withArrow && <div className='xl:scale-100 scale-75 ml-8'>
+            {withArrow && <div className='xl:scale-100 lg:scale-75 scale-[66%] xl:ml-8 lg:ml-6 ml-2'>
                 <Arrow />
             </div>}
         </button>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,22 +5,26 @@ import Link from "next/link";
 import { gsap } from "gsap";
 
 function openMenuTween() {
+	const body = document.querySelector('body');
+	body.style.overflow = 'hidden';
 	gsap.fromTo("[data-animation='menu']", { y: -400, opacity: 0 }, { y: 0, opacity: 1, duration: .2});
-  gsap.to("[data-animation='firstLine']", { y: 5, duration: .2});
-  gsap.to("[data-animation='secondLine']", { y: -5, duration: .2});
-  gsap.to("[data-animation='firstLine']", { rotate: 45, y: -4, x: .5, delay: .4, duration: .2});
-  gsap.to("[data-animation='secondLine']", { rotate: -45, y: 4, x: -.5, delay: .4, duration: .2});
+	gsap.to("[data-animation='firstLine']", { y: 5, duration: .2});
+	gsap.to("[data-animation='secondLine']", { y: -5, duration: .2});
+	gsap.to("[data-animation='firstLine']", { rotate: 45, y: -4, x: .5, delay: .4, duration: .2});
+	gsap.to("[data-animation='secondLine']", { rotate: -45, y: 4, x: -.5, delay: .4, duration: .2});
 }
 
 function closeMenuTween({ onComplete }) {
+	const body = document.querySelector('body');
+	body.style.overflow = 'visible';
 	gsap.to("[data-animation='menu']", { y: -400, opacity: 0, onComplete, duration: .2 });
-  gsap.to("[data-animation='firstLine']", { rotate: 0, y: 0, x: 0, duration: .2});
-  gsap.to("[data-animation='secondLine']", { rotate: 0, y: 0, x: 0, duration: .2});
-  gsap.to("[data-animation='firstLine']", { y: 0, delay: .4, duration: .2});
-  gsap.to("[data-animation='secondLine']", { y: 0, delay: .4, duration: .2});
+	gsap.to("[data-animation='firstLine']", { rotate: 0, y: 0, x: 0, duration: .2});
+	gsap.to("[data-animation='secondLine']", { rotate: 0, y: 0, x: 0, duration: .2});
+	gsap.to("[data-animation='firstLine']", { y: 0, delay: .4, duration: .2});
+	gsap.to("[data-animation='secondLine']", { y: 0, delay: .4, duration: .2});
 }
 
-export const Menu = () => {
+export const Menu = ({toggleMenu, ...props}) => {
 	useLayoutEffect(() => {
 		openMenuTween();
 	}, []);
@@ -50,7 +54,7 @@ export const Menu = () => {
 			className="xl:hidden w-full h-full fixed top-0 z-20 pt-36 pl-4 flex flex-col gap-16 items-start bg-white font-mabry text-xl uppercase"
 		>
 			{menuItems.map((menuItems, i) => (
-				<Link key={i} href={menuItems.link}>
+				<Link onClick={toggleMenu} key={i} href={menuItems.link}>
 					{menuItems.name}
 				</Link>
 			))}
@@ -72,7 +76,7 @@ export const Layout = ({ children }) => {
 	return (
 		<div>
 			<Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
-			{isMenuOpen && <Menu />}
+			{isMenuOpen && <Menu toggleMenu={toggleMenu} />}
 			{children}
 			<Footer />
 		</div>
