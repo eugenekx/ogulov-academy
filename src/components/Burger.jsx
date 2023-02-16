5641b44612fbb82e093bf6b7a34e9306b5d4cfeb
eugenekx <kx.eugene@gmail.com>
2023-02-16 23:19:27 +0400

    update mobile menu and burger

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,18 +1,28 @@
-function Burger({onClick}) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
-        onClick={onClick}
-        fill="none"
-        viewBox="0 0 32 32"
-      >
-        <path fill="transparent" d="M0 0H32V32H0z"></path>
-        <path data-animation='firstLine' fill="#000" d="M3 10H29V12H3z"></path>
-        <path data-animation='secondLine' fill="#000" d="M3 20H29V22H3z"></path>
-      </svg>
-    );
-  }
-  
-  export default Burger;
+function Burger({ onClick }) {
+	return (
+		<svg
+			width={32}
+			height={32}
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			id="burger-svg"
+			className="cursor-pointer lg:hidden"
+			onClick={onClick}
+		>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M4 21.3333C4 20.597 4.59695 20 5.33333 20L26.6667 20C27.403 20 28 20.597 28 21.3333C28 22.0697 27.403 22.6667 26.6667 22.6667L5.33333 22.6667C4.59695 22.6667 4 22.0697 4 21.3333Z"
+				fill="#161620"
+			/>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M4 10.6667C4 9.93033 4.59695 9.33337 5.33333 9.33337L26.6667 9.33337C27.403 9.33337 28 9.93033 28 10.6667C28 11.4031 27.403 12 26.6667 12L5.33333 12C4.59695 12 4 11.4031 4 10.6667Z"
+				fill="#161620"
+			/>
+		</svg>
+	);
+}
+
+export default Burger;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,59 +7,75 @@ import { gsap } from "gsap";
 function openMenuTween() {
 	const body = document.querySelector("body");
 	body.style.overflow = "hidden";
+	// gsap.fromTo(
+	// 	"[data-animation='menu']",
+	// 	{ y: -400, opacity: 0 },
+	// 	{ y: 0, opacity: 1, duration: 0.2 }
+	// );
+
 	gsap.fromTo(
 		"[data-animation='menu']",
-		{ y: -400, opacity: 0 },
-		{ y: 0, opacity: 1, duration: 0.2 }
+		{ background: "linear-gradient(180deg, white 0%, transparent 1%)" },
+		{
+			background: "linear-gradient(180deg, white 0%, white 100%)",
+			ease: "expo.inOut",
+			duration: 0.25,
+		}
 	);
-	gsap.to("[data-animation='firstLine']", { y: 5, duration: 0.2 });
-	gsap.to("[data-animation='secondLine']", { y: -5, duration: 0.2 });
-	gsap.to("[data-animation='firstLine']", {
-		rotate: 45,
-		y: -4,
-		x: 0.5,
-		delay: 0.4,
-		duration: 0.2,
+
+	gsap.fromTo(
+		"[data-animation='menu-link']",
+		{ yPercent: -50, opacity: 0 },
+		{
+			yPercent: 0,
+			opacity: 1,
+			stagger: 0.025,
+			delay: 0.1,
+			ease: "expo.inOut",
+		}
+	);
+
+	gsap.to("#burger-svg path:nth-child(1)", {
+		rotation: -45,
+		y: -6,
+		transformOrigin: "center center",
+		duration: 0.5,
+		ease: "expo.inOut",
 	});
-	gsap.to("[data-animation='secondLine']", {
-		rotate: -45,
-		y: 4,
-		x: -0.5,
-		delay: 0.4,
-		duration: 0.2,
+	gsap.to("#burger-svg path:nth-child(2)", {
+		rotation: 45,
+		y: 5,
+		transformOrigin: "center center",
+		duration: 0.5,
+		ease: "expo.inOut",
 	});
 }
 
 function closeMenuTween({ onComplete }) {
 	const body = document.querySelector("body");
 	body.style.overflow = "visible";
-	gsap.to("[data-animation='menu']", {
-		y: -400,
-		opacity: 0,
-		onComplete,
-		duration: 0.2,
-	});
-	gsap.to("[data-animation='firstLine']", {
-		rotate: 0,
+	gsap.fromTo(
+		"[data-animation='menu']",
+		{ opacity: 1 },
+		{
+			opacity: 0,
+			ease: "expo.inOut",
+			onComplete,
+		}
+	);
+	gsap.to("#burger-svg path:nth-child(1)", {
+		rotation: 0,
 		y: 0,
-		x: 0,
-		duration: 0.2,
+		transformOrigin: "center center",
+		duration: 0.5,
+		ease: "expo.inOut",
 	});
-	gsap.to("[data-animation='secondLine']", {
-		rotate: 0,
+	gsap.to("#burger-svg path:nth-child(2)", {
+		rotation: 0,
 		y: 0,
-		x: 0,
-		duration: 0.2,
-	});
-	gsap.to("[data-animation='firstLine']", {
-		y: 0,
-		delay: 0.4,
-		duration: 0.2,
-	});
-	gsap.to("[data-animation='secondLine']", {
-		y: 0,
-		delay: 0.4,
-		duration: 0.2,
+		transformOrigin: "center center",
+		duration: 0.5,
+		ease: "expo.inOut",
 	});
 }
 
@@ -111,17 +127,35 @@ export const Menu = ({ toggleMenu, ...props }) => {
 			data-animation="menu"
 			className="xl:hidden w-full h-full fixed top-0 z-20 pt-32 pl-4 flex flex-col gap-2 items-start bg-white font-mabry text-xl uppercase"
 		>
-			<div className="text-s xl:text-m mt-8 mb-2 uppercase">Меню</div>
+			<div
+				data-animation="menu-link"
+				className="text-s xl:text-m mt-8 mb-2 uppercase"
+			>
+				Меню
+			</div>
 			{menuItems.map((menuItems, i) => (
-				<Link onClick={toggleMenu} key={i} href={menuItems.link}>
+				<Link
+					data-animation="menu-link"
+					onClick={toggleMenu}
+					key={i}
+					href={menuItems.link}
+				>
 					{menuItems.name}
 				</Link>
 			))}
-			<div className="text-s xl:text-m mt-8 mb-2 uppercase">
+			<div
+				data-animation="menu-link"
+				className="text-s xl:text-m mt-8 mb-2 uppercase"
+			>
 				Личный кабинет
 			</div>
 			{profileItems.map((menuItems, i) => (
-				<Link onClick={toggleMenu} key={i} href={menuItems.link}>
+				<Link
+					data-animation="menu-link"
+					onClick={toggleMenu}
+					key={i}
+					href={menuItems.link}
+				>
 					{menuItems.name}
 				</Link>
 			))}
